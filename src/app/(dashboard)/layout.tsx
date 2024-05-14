1b14772d96151026e81375b097465c3338ab8138
Hadiru Colley <dirudeen22@gmail.com>
2024-05-15 01:50:40 +0300

    Add Navbar component to jsx

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Sidebar from "./_components/sidebar";
+import Navbar from "./_components/navbar";
 
 export default function DashboardLayout({
   children,
@@ -8,10 +9,13 @@ export default function DashboardLayout({
 }) {
   return (
     <section className="h-full">
+      <div className="md:pl-56 flex items-center fixed h-20 inset-y-0 z-50 w-full">
+          <Navbar />
+      </div>
       <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
         <Sidebar />
       </div>
-      <main className="md:pl-56">{children}</main>
+      <main className="md:pl-56 mt-20 h-full">{children}</main>
     </section>
   );
 }
