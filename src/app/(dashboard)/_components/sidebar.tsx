cfbe5233529eb17cd6d19d7373dfcdac1a9211a8
Hadiru Colley <dirudeen22@gmail.com>
2024-05-15 00:36:07 +0300

    Add SidebarRoutes component to Sidebar component

--- src/app/(dashboard)/_components/sidebar.tsx
+++ src/app/(dashboard)/_components/sidebar.tsx
@@ -1,9 +1,13 @@
 import Logo from "./logo";
+import SidebarRoutes from "./sidebarRoutes";
 
 export default function Sidebar(){
     return (<aside className="h-full border-r flex flex-col bg-white overflow-y-auto shadow-lg">
         <div className="p-6">
             <Logo />
+        </div>
+        <div className="flex flex-col w-full">
+            <SidebarRoutes />
         </div>
     </aside>)
 }
