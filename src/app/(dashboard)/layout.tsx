9efe0f451359f90bf69f5548548d3cac7664ee41
Hadiru Colley <dirudeen22@gmail.com>
2024-05-14 22:59:18 +0300

    Uncomment and import sidebar component

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Sidebar from "./_components/sidebar";
 
 export default function DashboardLayout ({children}: {children: Readonly<React.ReactNode>}){
     return (
         <section className="h-full">
             <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0">
-                {/* <Sidebar> */}
+                <Sidebar />
             </div>
             {children}
         </section>
