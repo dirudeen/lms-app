10b5e5c28097984e99e1314f26747f77a52d6e5e
Hadiru Colley <dirudeen22@gmail.com>
2024-05-15 01:31:11 +0300

    Wrap the children with main element

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Sidebar from "./_components/sidebar";
 
-export default function DashboardLayout ({children}: {children: Readonly<React.ReactNode>}){
-    return (
-        <section className="h-full">
-            <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
-                <Sidebar />
-            </div>
-            {children}
-        </section>
-    )
+export default function DashboardLayout({
+  children,
+}: {
+  children: Readonly<React.ReactNode>;
+}) {
+  return (
+    <section className="h-full">
+      <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
+        <Sidebar />
+      </div>
+      <main className="md:pl-56">{children}</main>
+    </section>
+  );
 }
