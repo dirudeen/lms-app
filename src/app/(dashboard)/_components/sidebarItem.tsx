eaef91ea0c3d82e61f7ef92e09dfac79aa6aabde
Hadiru Colley <dirudeen22@gmail.com>
2024-05-15 01:23:52 +0300

    Modify the parent element to Link instead of a button

--- src/app/(dashboard)/_components/sidebarItem.tsx
+++ src/app/(dashboard)/_components/sidebarItem.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button"
 import { cn } from "@/lib/utils"
 import { LucideIcon } from "lucide-react"
 import Link from "next/link"
-import { usePathname } from "next/navigation"
+import { usePathname, useRouter } from "next/navigation"
 
 interface SidebarItemProp {
     icon: LucideIcon,
@@ -15,21 +15,19 @@ export default function SidebarItem({icon: Icon, path, label }: SidebarItemProp)
 
     const pathname = usePathname()
 
-    const isActive = (path === "/" && path === "/") || pathname === path || pathname.startsWith(`${path}/`)
+    const isActive = (pathname === "/" && path === "/") || pathname === path || pathname.startsWith(`${path}/`)
 
-    // todo check the markup
   return (
-    <button className={cn("flex items-center gap-x-2 text-slate-500 text-sm font-medium transition-all pl-6 hover:text-slate-600 hover:bg-slate-300/20", 
+    <Link href={path} className={cn("flex items-center gap-x-2 text-slate-500 text-sm font-medium transition-all pl-6 hover:text-slate-600 hover:bg-slate-300/20", 
         isActive && "text-sky-700 bg-slate-200/20 hover:text-sky-700 hover:bg-slate-200/20 h-full"
     )}>
         <div className="flex gap-x-2 items-center py-4">
             <Icon />
-            <Link href={path}>{label}</Link>
+            <p>{label}</p>
         </div>
         <div 
             className={cn("h-full ml-auto border-2 border-sky-700  opacity-0 transition-all", isActive && "opacity-100")}
         />
-
-    </button>
+    </Link>
   )
 }
