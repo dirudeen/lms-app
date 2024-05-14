ff0a4c532881b5614469d5ca76424bb4e9446129
Hadiru Colley <dirudeen22@gmail.com>
2024-05-14 23:05:09 +0300

    Add z index to sidebar wrapper element

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -4,7 +4,7 @@ import Sidebar from "./_components/sidebar";
 export default function DashboardLayout ({children}: {children: Readonly<React.ReactNode>}){
     return (
         <section className="h-full">
-            <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0">
+            <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
                 <Sidebar />
             </div>
             {children}
