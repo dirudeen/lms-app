be423c802b845af58f7ec4c15cc2444e74417136
Hadiru Colley <dirudeen22@gmail.com>
2024-05-15 02:02:40 +0300

    Add Sheet components for triggering and showing content

--- src/app/(dashboard)/_components/mobileSidebar.tsx
+++ src/app/(dashboard)/_components/mobileSidebar.tsx
@@ -1,5 +1,16 @@
+import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet";
 import { Menu } from "lucide-react";
+import Sidebar from "./sidebar";
 
-export default function MobileSidebar (){
-    return <Menu />
+export default function MobileSidebar() {
+  return (
+    <Sheet>
+      <SheetTrigger className="md:hidden pr-4 hover:opacity-50">
+        <Menu />
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0 bg-white">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
 }
