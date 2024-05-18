d0dadd8446432664a5c8778a3b1952050ef8d9a5
Hadiru Colley <dirudeen22@gmail.com>
2024-05-18 20:55:33 +0300

    Add transition class to sheetTrigger component

--- src/app/(dashboard)/_components/mobileSidebar.tsx
+++ src/app/(dashboard)/_components/mobileSidebar.tsx
@@ -5,7 +5,7 @@ import Sidebar from "./sidebar";
 export default function MobileSidebar() {
   return (
     <Sheet>
-      <SheetTrigger className="md:hidden pr-4 hover:opacity-50">
+      <SheetTrigger className="md:hidden pr-4 hover:opacity-50 transition">
         <Menu />
       </SheetTrigger>
       <SheetContent side="left" className="p-0 bg-white">
