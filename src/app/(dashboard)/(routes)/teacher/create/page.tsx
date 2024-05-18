20e5ef7cd0197bc1499634d0d7e874575c730ac3
Hadiru Colley <dirudeen22@gmail.com>
2024-05-18 21:22:51 +0300

    Create a create courses page component

---
+++ src/app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -0,0 +1,21 @@
+"use client"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+ 
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+export default function CreatePage() {
+  return (
+    <div className="max-w-5xl mx-auto md:flex md:items-center md:justify-center">CreatePage</div>
+  )
+}
