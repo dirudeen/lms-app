2be0e5819aa7525eb39e54d0593284bbef5d70bd
Hadiru Colley <dirudeen22@gmail.com>
2024-05-18 19:44:09 +0300

    Import NavbarRoutes and append it after MobileSidebar

--- src/app/(dashboard)/_components/navbar.tsx
+++ src/app/(dashboard)/_components/navbar.tsx
@@ -1,9 +1,11 @@
+import NavbarRoutes from "@/components/navbarRoutes"
 import MobileSidebar from "./mobileSidebar"
 
 export default function Navbar() {
   return (
     <div className="flex items-center p-4 border-b h-full shadow-sm w-full">
         <MobileSidebar />
+        <NavbarRoutes/>
     </div>
   )
 }
