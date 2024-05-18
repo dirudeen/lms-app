39b2c58aa9da93acb5cea13c5341e13cff7537cf
Hadiru Colley <dirudeen22@gmail.com>
2024-05-18 23:03:38 +0300

    Add Link component from next link

--- src/app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ src/app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,5 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function CoursesPage() {
   return (
-    <div>coursesPage</div>
+    <div className="p-6">
+      <Button asChild>
+        <Link href="/teacher/create">New Course</Link>
+      </Button>
+    </div>
   )
 }
