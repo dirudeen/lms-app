56a3200c62a08ceb7b9f600e8ac1aa9f262c2304
Hadiru Colley <dirudeen22@gmail.com>
2024-05-19 01:44:33 +0300

    Wrap the Image with next link component

--- src/app/(dashboard)/_components/logo.tsx
+++ src/app/(dashboard)/_components/logo.tsx
@@ -1,13 +1,16 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Logo() {
   return (
-    <Image 
-    src="/assets/logo.svg"
-    alt="Logo"
-    width={400}
-    height={400}
-    priority
-    />
+    <Link href="/">
+      <Image 
+      src="/assets/logo.svg"
+      alt="Logo"
+      width={400}
+      height={400}
+      priority
+      />
+    </Link>
   )
 }
