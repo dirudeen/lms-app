3ffaa6977f63a6d7106b7d5c1370d60ff6d9b033
Hadiru Colley <dirudeen22@gmail.com>
2024-05-18 20:30:55 +0300

    Add button to switch between teacher mode and guest mode

--- src/components/navbarRoutes.tsx
+++ src/components/navbarRoutes.tsx
@@ -1,9 +1,26 @@
-import { UserButton } from "@clerk/nextjs"
+"use client";
+import { UserButton } from "@clerk/nextjs";
+import { usePathname, useRouter } from "next/navigation";
+import { Button } from "./ui/button";
+import { LogOut } from "lucide-react";
+import Link from "next/link";
 
 export default function NavbarRoutes() {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const isTeacherPage = pathname.startsWith("/teacher");
+  const isPlayerPage = pathname.includes("/chapter");
   return (
     <div className="flex gap-x-2 ml-auto">
-        <UserButton />
+    <Button asChild variant='ghost' size="sm">
+      {isTeacherPage || isPlayerPage ? (
+        <Link href="/" className="space-x-2"><LogOut className="size-4 mr-2" /> Exit</Link>
+      ) : (
+          <Link href="/teacher/courses">Teacher mode</Link>
+        )}
+    </Button>
+      <UserButton />
     </div>
-  )
+  );
 }
