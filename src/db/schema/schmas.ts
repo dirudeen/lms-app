ebdfc150bd03fcab12043e646a1de68b98630259
Hadiru Colley <dirudeen22@gmail.com>
2024-06-04 02:01:40 +0300

    Modified schema.ts file
    Create a category table and add a categoryId to course table for referencing

--- src/db/schema/schmas.ts
+++ src/db/schema/schmas.ts
@@ -11,24 +11,30 @@ export const course = pgTable('Course', {
   imageUrl: text("imageUrl"),
   price: numeric('price', {precision: 12, scale: 2}),
   isPublished: boolean("isPublished").default(false),
-  categoryId: text("categoryId"),
 
-  attachment: text("attachment"),
+  categoryId: uuid("categoryId").notNull().references(() => category.id),
+
   createdAt: timestamp("createdAt").defaultNow().notNull(),
   updatedAt: timestamp("updatedAt").defaultNow().notNull()
-  
-  
 });
+
+export const category = pgTable("Category", {
+  id: uuid("id").primaryKey().default(sql`gen_random_uuid()`),
+  name: text("name").notNull(),
+})
+
+
+export const attachment = pgTable("Attachment", {
+    id: uuid("id").primaryKey().default(sql`gen_random_uuid()`),
+    courseId: uuid("courseId").notNull().references(() => course.id, {onDelete: "cascade"}),
+    url: text("url"),
+    createdAt: timestamp("createdAt").defaultNow().notNull(),
+    updatedAt: timestamp("updatedAt").defaultNow().notNull()
+
+})
+
 // Schema for inserting a course 
 export const insertCourseSchema = createInsertSchema(course, {
   title: z.string().min(1, {message: "Title must be at least 3 characters"}),
   userId: z.string()
 })
-
-export const attachment = pgTable("Attachments", {
-    id: uuid("id").primaryKey().default(sql`gen_random_uuid()`),
-    courseId: uuid("courseId").notNull().references(() => course.id, {onDelete: "cascade"}),
-    url: text("url")
-
-})
-
