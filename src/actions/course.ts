e3670a3809ed5fa0e2a1b021e0aa713a458d338a
Hadiru Colley <dirudeen22@gmail.com>
2024-06-03 23:53:08 +0300

    Add logic to insert data into the course table

--- src/actions/course.ts
+++ src/actions/course.ts
@@ -1,6 +1,10 @@
 "use server";
-
+import { db } from "@/db";
+import { course as courseTable } from "@/db/schema/schmas";
 import { auth } from "@clerk/nextjs/server";
+import { insertCourseSchema } from "@/db/schema/schmas";
+
+
 
 const { userId } = auth();
 
@@ -8,9 +12,18 @@ export async function createCourse({ title }: { title: string }) {
   if (!userId) {
     throw new Error("Unautherized");
   }
-
+  // validate the title and userId before inserting to db
+  const validatedCourseDraft = insertCourseSchema.parse({
+    title,
+    userId
+  })
   try {
-    const course = {};
+    const course = await db
+    .insert(courseTable)
+    .values(validatedCourseDraft)
+    .returning()
+    .then(res => res[0])
+
     return course;
   } catch (error) {
     console.log(["COURSE CREATION", error]);
