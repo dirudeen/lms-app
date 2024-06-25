232dc3e093c6011ed19d1fa01ba13f14d5f16f5d
Hadiru Colley <dirudeen22@gmail.com>
2024-06-25 20:37:21 +0300

    Create a fetchCourse server action

--- src/actions/course.ts
+++ src/actions/course.ts
@@ -3,12 +3,13 @@ import { db } from "@/db";
 import { course as courseTable } from "@/db/schema/schmas";
 import { auth } from "@clerk/nextjs/server";
 import { insertCourseSchema } from "@/db/schema/schmas";
+import { eq, and } from "drizzle-orm";
+import { redirect } from "next/navigation";
 
 
-
-const { userId } = auth();
 
 export async function createCourse({ title }: { title: string }) {
+  const { userId } = auth();
   if (!userId) {
     throw new Error("Unautherized");
   }
@@ -29,5 +30,29 @@ export async function createCourse({ title }: { title: string }) {
   } catch (error) {
     console.log(["COURSE CREATION", error]);
     throw new Error("Failed to create a course");
+  }
+}
+
+
+export async function fetchCourse(courseId: string) {
+  const { userId } = auth();
+  if (!userId) {
+    throw new Error("Unautherized");
+  }
+
+  try {
+    const course = await db
+    .select()
+    .from(courseTable)
+    .where(and(eq(courseTable.userId, userId), eq(courseTable.id, courseId)))
+    .then(res => res[0])
+
+    if (!course) {
+      redirect("/")
+    }
+    return course;
+  } catch (error) {
+    console.log(["GET COURSES", error]);
+    throw new Error("Failed to get courses");
   }
 }
