509bf3341427d5e9a762d9a8304a1220c69a902c
Hadiru Colley <dirudeen22@gmail.com>
2024-06-25 20:38:07 +0300

    Get a Course
    Fetch a course using the courseId and render the number the number of fields that should be completed

--- src/app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ src/app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,11 +1,29 @@
+import { fetchCourse } from "@/actions/course"
+
 interface Props {
     params:{
         courseId: string
     }
 }
 
-export default function CoursePage({params: {courseId}}: Props) {
+
+export default async function CoursePage({params: {courseId}}: Props) {
+  const course = await fetchCourse(courseId)
+
+  // fields to complete
+  const requiredFields = [course.title, course.description, course.imageUrl, course.categoryId]
+  const totalFields = requiredFields.length
+  const completedFields = requiredFields.filter(Boolean).length
+  const completionText = `${completedFields} / ${totalFields}`
+
   return (
-    <div>Course detail page of id {courseId}</div>
+    <div className="p-6">
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col gap-y-2">
+          <h1 className="text-2xl font-medium">Course setup</h1>
+          <span className="text-sm text-slate-700">Complete all fields {completionText}</span>
+        </div>
+      </div>
+    </div>
   )
 }
