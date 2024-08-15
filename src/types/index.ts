f6bfc738461589255721f7bdc580e94a7ad1cdd2
Hadiru Colley <dirudeen22@gmail.com>
2024-08-16 01:16:52 +0300

    Create imageUrlinputvalidation object

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,6 +4,7 @@ import { z } from "zod"
 // pick a title only from the insertCourseSchema
 export const titleInputValidation = insertCourseSchema.pick({title: true})
 export const descriptionInputValidation = insertCourseSchema.pick({description: true})
+export const imageUrlInputValidation = insertCourseSchema.pick({imageUrl: true,})
 
 const updateCourse = insertCourseSchema.omit({userId: true})
 export type Course = z.infer<typeof updateCourse>
