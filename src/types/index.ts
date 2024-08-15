7c4e0b51a3863b19618835cad16d6f3d82b32ff3
Hadiru Colley <dirudeen22@gmail.com>
2024-08-15 20:14:39 +0300

    Create a Course type definition for updating a course

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,9 @@
 import {insertCourseSchema} from "@/db/schema/schmas" 
+import { z } from "zod"
 
 // pick a title only from the insertCourseSchema
 export const titleInputValidation = insertCourseSchema.pick({title: true})
+export const descriptionInputValidation = insertCourseSchema.pick({description: true})
+
+const updateCourse = insertCourseSchema.omit({userId: true})
+export type Course = z.infer<typeof updateCourse>
