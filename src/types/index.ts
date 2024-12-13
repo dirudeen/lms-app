0830085974f71594fbbef6de95234801e4c081b2
dirudeen <dirudeen22@gmail.com>
2024-12-13 20:21:21 +0300

    Export Attachment type and format with prettier

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,12 +1,23 @@
-import {insertCourseSchema} from "@/db/schema/schmas" 
-import { z } from "zod"
+import {
+  attachment as attachmentTable,
+  course as courseTable,
+  insertCourseSchema,
+} from "@/db/schema/schmas";
 
 // pick a title only from the insertCourseSchema
-export const titleInputValidation = insertCourseSchema.pick({title: true})
-export const descriptionInputValidation = insertCourseSchema.pick({description: true})
-export const imageUrlInputValidation = insertCourseSchema.pick({imageUrl: true,})
-export const categoryInputValidation = insertCourseSchema.pick({categoryId: true})
-export const priceInputValidation = insertCourseSchema.pick({price: true})
+export const titleInputValidation = insertCourseSchema.pick({ title: true });
+export const descriptionInputValidation = insertCourseSchema.pick({
+  description: true,
+});
+export const imageUrlInputValidation = insertCourseSchema.pick({
+  imageUrl: true,
+});
+export const categoryInputValidation = insertCourseSchema.pick({
+  categoryId: true,
+});
+export const priceInputValidation = insertCourseSchema.pick({ price: true });
 
-const updateCourse = insertCourseSchema.omit({userId: true})
-export type Course = z.infer<typeof updateCourse>
+export type Course = typeof courseTable.$inferSelect;
+
+// attachment select type
+export type Attachment = typeof attachmentTable.$inferSelect;
