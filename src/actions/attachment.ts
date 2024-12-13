b8cd9ce32a4a7e8cb387358d30595c7471760a3a
dirudeen <dirudeen22@gmail.com>
2024-12-13 23:14:53 +0300

    Add a deleteAttachment server action for deleting an attachment

--- src/actions/attachment.ts
+++ src/actions/attachment.ts
@@ -9,6 +9,7 @@ import {
 } from "@/db/schema/schmas";
 import { eq } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
+import { deleteUTFile } from "./uploadthing-action";
 
 interface CreateAttachmentProps {
   courseId: string;
@@ -47,4 +48,38 @@ export async function createAttachment({
     console.log("CREATE ATTACHMENT ERROR", error);
     throw new Error("Error creating attachment");
   }
+}
+
+interface DeleteAttachmentProps {
+  attachmentId: string;
+  courseId: string;
+  path: string;
+  url: string;
+}
+
+export async function deleteAttachment({attachmentId, courseId, path, url }: DeleteAttachmentProps) {
+  const { userId } = auth();
+  if (!userId) {
+    throw new Error("Unautherized");
+  }
+
+  try {
+    const { courseOwner } = await db
+      .select({ courseOwner: courseTable.userId })
+      .from(courseTable)
+      .where(eq(courseTable.id, courseId))
+      .then((res) => res[0]);
+
+    if (courseOwner !== userId) {
+      throw new Error("Course not found");
+    }
+    await db.delete(attachmentTable).where(eq(attachmentTable.id, attachmentId));
+    await deleteUTFile(url);
+    revalidatePath(path);
+    return;
+} catch (error) {
+    console.log("DELETE ATTACHMENT ERROR", error);
+    throw new Error("Error deleting attachment");
+}
+
 }
