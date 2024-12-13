2ec6a0b18b292c537749cf72d224e32f6d04ac7c
dirudeen <dirudeen22@gmail.com>
2024-12-13 20:22:34 +0300

    Modify onchange func to optionnaly recieve the name of file

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -5,7 +5,7 @@ import toast from "react-hot-toast";
 
 interface FileUploadProps {
     endpoint: keyof typeof ourFileRouter
-    onchange: (url?: string) => void
+    onchange: (url?: string, name?: string) => void
 }
 
 export function FileUpload({endpoint, onchange}: FileUploadProps){
@@ -13,7 +13,7 @@ export function FileUpload({endpoint, onchange}: FileUploadProps){
     return <UploadDropzone
         endpoint={endpoint}
         onClientUploadComplete={(res) => {
-            onchange(res?.[0].url)
+            onchange(res?.[0]?.url, res?.[0]?.name)
         }}
         onUploadError={(error: Error) => {
             toast.error(error.message)
