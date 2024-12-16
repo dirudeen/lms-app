f77a268943d6019866bb3e80691d202440c18d59
dirudeen <dirudeen22@gmail.com>
2024-12-16 19:54:06 +0300

    Add className props to extend the classes

--- src/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/FormCard.tsx
+++ src/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/FormCard.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { ReactNode } from 'react'
 
-export default function FormCard({children}: {children: ReactNode}) {
+export default function FormCard({children, className}: {children: ReactNode, className?: string}) {
   return (
     <div
-    className="bg-slate-100 p-4 mt-6 rounded-md border"
+    className={`bg-slate-100 p-4 mt-6 rounded-md border ${className}`}
     >{children}</div>
   )
 }
