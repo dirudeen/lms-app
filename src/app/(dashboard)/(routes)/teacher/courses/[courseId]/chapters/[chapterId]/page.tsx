0b2d57927109a5e86e192f061d9ff134ee0e1019
dirudeen <dirudeen22@gmail.com>
2024-12-17 19:21:35 +0300

    Import and render the ChapterAccessForm component

--- src/app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ src/app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,10 +1,11 @@
 import { fetchChapter } from "@/actions/chapters";
 import { IconBadge } from "@/components/IconBadge";
-import { ArrowLeft, LayoutDashboard } from "lucide-react";
+import { ArrowLeft, Eye, LayoutDashboard } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 import { ChapterTitleForm } from "./_components/chapterTitleForm";
 import { ChapterDescriptionForm } from "./_components/chapterDescriptionForm";
+import { ChapterAccessForm } from "./_components/chapterAccessForm";
 
 interface Props {
   params: {
@@ -16,16 +17,12 @@ interface Props {
 export default async function ChapterPage({
   params: { chapterId, courseId },
 }: Props) {
-  const chapter = await fetchChapter({chapterId, courseId})
+  const chapter = await fetchChapter({ chapterId, courseId });
 
-  const requiredFields = [
-    chapter.title,
-    chapter.description,
-    chapter.videoUrl,
-  ]
-  const requiredFieldsLength = requiredFields.length
-  const completedFields = requiredFields.filter(Boolean).length
-  const completionText = `(${completedFields} / ${requiredFieldsLength})`
+  const requiredFields = [chapter.title, chapter.description, chapter.videoUrl];
+  const requiredFieldsLength = requiredFields.length;
+  const completedFields = requiredFields.filter(Boolean).length;
+  const completionText = `(${completedFields} / ${requiredFieldsLength})`;
 
   return (
     <div className="p-6">
@@ -40,22 +37,43 @@ export default async function ChapterPage({
           </Link>
           <div className="flex items-center justify-between w-full">
             <div className="flex flex-col gap-y-2">
-                <h1 className="text-2xl font-medium">Chapter Creation</h1>
-                <span className="text-sm text-slate-700">
-                  Complete all fields {completionText}
-                </span>
+              <h1 className="text-2xl font-medium">Chapter Creation</h1>
+              <span className="text-sm text-slate-700">
+                Complete all fields {completionText}
+              </span>
             </div>
           </div>
         </div>
       </div>
       <section className="grid grid-cols-1 md:grid-cols-2 mt-16 gap-6">
-        <div className="space-y-4"> 
+        <div className="space-y-4">
+          <div className="space-y-4">
             <div className="flex items-center gap-2">
-                <IconBadge icon={LayoutDashboard} />
-                <h2 className="text-xl ">Customize your chapter</h2>
+              <IconBadge icon={LayoutDashboard} />
+              <h2 className="text-xl ">Customize your chapter</h2>
             </div>
-            <ChapterTitleForm initialData={chapter} courseId={courseId} chapterId={chapterId} />
-            <ChapterDescriptionForm initialData={chapter} courseId={courseId} chapterId={chapterId} />
+            <ChapterTitleForm
+              initialData={chapter}
+              courseId={courseId}
+              chapterId={chapterId}
+            />
+            <ChapterDescriptionForm
+              initialData={chapter}
+              courseId={courseId}
+              chapterId={chapterId}
+            />
+          </div>
+          <div className="space-y-4">
+            <div className="flex items-center gap-2">
+              <IconBadge icon={Eye} />
+              <h2 className="text-xl ">Access Settings</h2>
+            </div>
+            <ChapterAccessForm
+              initialData={chapter}
+              courseId={courseId}
+              chapterId={chapterId}
+            />
+          </div>
         </div>
       </section>
     </div>
