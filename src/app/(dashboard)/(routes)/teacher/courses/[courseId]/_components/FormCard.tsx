e1b7ba13f4676834ab93a9cb0ad8a0429c52edb7
dirudeen <dirudeen22@gmail.com>
2024-12-17 16:37:34 +0300

    Add shadow class to wrapper div

--- src/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/FormCard.tsx
+++ src/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/FormCard.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from 'react'
 export default function FormCard({children, className}: {children: ReactNode, className?: string}) {
   return (
     <div
-    className={`bg-slate-100 p-4 mt-6 rounded-md border ${className}`}
+    className={`bg-slate-100 p-4 mt-6 rounded-md border shadow-sm ${className}`}
     >{children}</div>
   )
 }
