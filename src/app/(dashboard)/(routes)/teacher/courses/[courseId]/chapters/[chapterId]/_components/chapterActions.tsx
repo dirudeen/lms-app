28c70427614618470e3bf3fca442ff987117eeab
dirudeen <dirudeen22@gmail.com>
2024-12-18 17:00:53 +0300

    Add onClickHandler for publishing and unpublishing a chapter

--- src/app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapterActions.tsx
+++ src/app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapterActions.tsx
@@ -1,10 +1,14 @@
 "use client";
-import { deleteChapter } from "@/actions/chapters";
+import {
+    deleteChapter,
+    publishChapter,
+    unpublishChapter,
+} from "@/actions/chapters";
 import { ConfirmModal } from "@/components/modals/confirmModal";
 import { Spinner } from "@/components/Spinner";
 import { Button } from "@/components/ui/button";
 import { Trash } from "lucide-react";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { useState } from "react";
 import toast from "react-hot-toast";
 
@@ -22,10 +26,33 @@ export default function ChapterActions({
   isPublished,
 }: ChapterActionsProps) {
   const [isLoading, setIsLoading] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+
   const router = useRouter();
+  const path = usePathname();
+
+  const onClickHandler = async () => {
+    setIsLoading(true);
+
+    try {
+      if (isPublished) {
+        await unpublishChapter({ courseId, chapterId, path });
+        toast.success("Chapter unpublished successfully");
+      } else {
+        await publishChapter({ courseId, chapterId, path });
+        toast.success("Chapter published successfully");
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        toast.error(error.message);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   const onDeleteChapter = async () => {
-    setIsLoading(true);
+    setIsDeleting(true);
 
     try {
       await deleteChapter({ courseId, chapterId });
@@ -36,18 +63,27 @@ export default function ChapterActions({
         toast.error(error.message);
       }
     } finally {
-      setIsLoading(false);
+      setIsDeleting(false);
     }
   };
 
   return (
     <div className="flex items-center gap-2">
-      <Button variant={"outline"} disabled={disabled}>
-        {isPublished ? "Unpublish" : "Publish"}
+      <Button
+        variant={"outline"}
+        disabled={disabled || isLoading || isDeleting}
+        onClick={onClickHandler}
+      >
+        {isPublished && !isLoading ? "Unpublish" : "Publish"}
+        {isLoading && <Spinner />}
       </Button>
       <ConfirmModal onConfirm={onDeleteChapter}>
-        <Button variant={"destructive"} size={"sm"} disabled={isLoading}>
-          {isLoading ? <Spinner /> : <Trash />}
+        <Button
+          variant={"destructive"}
+          size={"sm"}
+          disabled={isDeleting || isLoading}
+        >
+          {isDeleting ? <Spinner /> : <Trash />}
         </Button>
       </ConfirmModal>
     </div>
