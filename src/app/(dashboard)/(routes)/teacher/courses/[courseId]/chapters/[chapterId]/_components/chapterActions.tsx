26e23b674823f4463e6258f3e36a8010e992cc63
dirudeen <dirudeen22@gmail.com>
2024-12-18 16:15:44 +0300

    Create the onDeleteChapter function for handling the onConfirm prop

--- src/app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapterActions.tsx
+++ src/app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapterActions.tsx
@@ -1,7 +1,12 @@
-"use client"
+"use client";
+import { deleteChapter } from "@/actions/chapters";
 import { ConfirmModal } from "@/components/modals/confirmModal";
+import { Spinner } from "@/components/Spinner";
 import { Button } from "@/components/ui/button";
 import { Trash } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 interface ChapterActionsProps {
   courseId: string;
@@ -10,17 +15,41 @@ interface ChapterActionsProps {
   isPublished: boolean;
 }
 
-export default function ChapterActions({courseId, chapterId, disabled, isPublished}: ChapterActionsProps) {
+export default function ChapterActions({
+  courseId,
+  chapterId,
+  disabled,
+  isPublished,
+}: ChapterActionsProps) {
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+
+  const onDeleteChapter = async () => {
+    setIsLoading(true);
+
+    try {
+      await deleteChapter({ courseId, chapterId });
+      toast.success("Chapter deleted successfully");
+      router.push(`/teacher/courses/${courseId}`);
+    } catch (error) {
+      if (error instanceof Error) {
+        toast.error(error.message);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <div className="flex items-center gap-2">
-        <Button variant={'outline'} disabled={disabled}>
-            {isPublished ? 'Unpublish' : "Publish"} 
+      <Button variant={"outline"} disabled={disabled}>
+        {isPublished ? "Unpublish" : "Publish"}
+      </Button>
+      <ConfirmModal onConfirm={onDeleteChapter}>
+        <Button variant={"destructive"} size={"sm"} disabled={isLoading}>
+          {isLoading ? <Spinner /> : <Trash />}
         </Button>
-        <ConfirmModal onConfirm={() => {}}>
-        <Button variant={'destructive'} size={"sm"}>
-            <Trash />
-        </Button>
-        </ConfirmModal>
+      </ConfirmModal>
     </div>
-  )
+  );
 }
