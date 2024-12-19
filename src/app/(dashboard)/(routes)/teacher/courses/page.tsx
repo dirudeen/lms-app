d5622704a549617ac692df850975d7307d85348b
dirudeen <dirudeen22@gmail.com>
2024-12-19 15:54:33 +0300

    Setup the data-table and render it with dummy data

--- src/app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ src/app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,12 +1,43 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/column";
 
-export default function CoursesPage() {
+type Payment = {
+  id: string
+  amount: number
+  status: "pending" | "processing" | "success" | "failed"
+  email: string
+}
+
+const payments: Payment[] = [
+  {
+    id: "728ed52f",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  },
+  {
+    id: "489e1d42",
+    amount: 125,
+    status: "processing",
+    email: "example@gmail.com",
+  },
+]
+
+ async function getData():Promise<Payment[]> { 
+   return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(payments)
+      }, 1000)
+    })
+  }
+
+export default async function CoursesPage() {
+const data = await getData()
   return (
     <div className="p-6">
-      <Button asChild>
-        <Link href="/teacher/create">New Course</Link>
-      </Button>
+      <DataTable columns={columns} data={data} />
     </div>
   )
 }
