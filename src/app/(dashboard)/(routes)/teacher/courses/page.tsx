cb0efc23508949661c385a5a1fbf741f343b00dd
dirudeen <dirudeen22@gmail.com>
2024-12-19 16:13:51 +0300

    fetch the courses and pass the result to the Data Table component

--- src/app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ src/app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -2,42 +2,13 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { DataTable } from "./_components/data-table";
 import { columns } from "./_components/column";
-
-type Payment = {
-  id: string
-  amount: number
-  status: "pending" | "processing" | "success" | "failed"
-  email: string
-}
-
-const payments: Payment[] = [
-  {
-    id: "728ed52f",
-    amount: 100,
-    status: "pending",
-    email: "m@example.com",
-  },
-  {
-    id: "489e1d42",
-    amount: 125,
-    status: "processing",
-    email: "example@gmail.com",
-  },
-]
-
- async function getData():Promise<Payment[]> { 
-   return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(payments)
-      }, 1000)
-    })
-  }
+import { fetchAllCourses } from "@/actions/course";
 
 export default async function CoursesPage() {
-const data = await getData()
+  const courses = await fetchAllCourses();
   return (
     <div className="p-6">
-      <DataTable columns={columns} data={data} />
+      <DataTable columns={columns} data={courses} />
     </div>
-  )
+  );
 }
