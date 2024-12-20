33e0205b6b8e717234b81bb58c789de1b956cdba
dirudeen <dirudeen22@gmail.com>
2024-12-20 19:43:03 +0300

    Add CoursesList to SearchPage for displaying filtered courses

--- src/app/(dashboard)/(routes)/search/page.tsx
+++ src/app/(dashboard)/(routes)/search/page.tsx
@@ -1,17 +1,31 @@
-import { fetchCategories } from "@/actions/course";
+import {
+  fetchCategories,
+  getCoursesWithProgressAndCategory,
+} from "@/actions/course";
 import Categories from "./_components/categories";
 import SearchInput from "@/components/seaerchInput";
+import CoursesList from "@/components/coursesList";
 
-export default async function SearchPage() {
+interface SearchPageProps {
+  searchParams: {
+    title: string;
+    categoryId: string;
+  };
+}
+
+export default async function SearchPage({ searchParams }: SearchPageProps) {
   const categories = await fetchCategories();
+  const courses = await getCoursesWithProgressAndCategory(searchParams);
+
   return (
     <>
-    <div className="px-6 pt-6 block md:hidden">
-    <SearchInput />
-    </div>
-    <div className="p-6">
-      <Categories items={categories} />
-    </div>
+      <div className="px-6 pt-6 block md:hidden">
+        <SearchInput />
+      </div>
+      <div className="p-6">
+        <Categories items={categories} />
+        <CoursesList items={courses} />
+      </div>
     </>
   );
 }
