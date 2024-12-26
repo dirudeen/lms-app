e2c56d4ac6f2769f2e3f191e119ed86bbcdea8ac
dirudeen <dirudeen22@gmail.com>
2024-12-26 18:05:46 +0300

    Upate the update the user progress when the botton is clicked

--- src/app/(course)/courses/[courseId]/chapters/[chapterId]/_components/courseProgressButton.tsx
+++ src/app/(course)/courses/[courseId]/chapters/[chapterId]/_components/courseProgressButton.tsx
@@ -1,7 +1,12 @@
 "use client";
 
+import { updateUserProgress } from "@/actions/userProgress";
 import { Button } from "@/components/ui/button";
+import { useConfettiStore } from "@/hooks/use-confetti-store";
 import { CheckCircle, XCircle } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 interface CourseProgressButtonProps {
   courseId: string;
@@ -10,21 +15,46 @@ interface CourseProgressButtonProps {
   isCompleted?: boolean;
 }
 
-
 export default function CourseProgressButton({
   courseId,
   chapterId,
   nextChapterId,
   isCompleted,
 }: CourseProgressButtonProps) {
+  const Icon = isCompleted ? XCircle : CheckCircle;
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+  const path = usePathname();
+  const confetti = useConfettiStore();
 
-    const Icon = isCompleted ? XCircle : CheckCircle;
-
-return <Button
-    type="button"
-    variant={isCompleted ? "outline" : "success"}
+  const clickHandler = async () => {
+    setIsLoading(true);
+    try {
+      await updateUserProgress({ courseId, chapterId, isCompleted, path });
+      if (!isCompleted && !nextChapterId) {
+        confetti.onOpen();
+      }
+      if (!isCompleted && nextChapterId) {
+        router.push(`/courses/${courseId}/chapters/${nextChapterId}`);
+      }
+      toast.success("Progress updated");
+    } catch (error) {
+      if (error instanceof Error) {
+        toast.error(error.message);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return (
+    <Button
+      onClick={clickHandler}
+      disabled={isLoading}
+      type="button"
+      variant={isCompleted ? "outline" : "success"}
     >
-    {isCompleted ? "Not Completed" : "Mark as Complete"}
-    <Icon className="size-4 ml-2" />
-  </Button>;
+      {isCompleted ? "Not Completed" : "Mark as Complete"}
+      <Icon className="size-4 ml-2" />
+    </Button>
+  );
 }
