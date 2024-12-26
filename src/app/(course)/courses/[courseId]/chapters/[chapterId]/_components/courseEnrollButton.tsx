7f93b507c63ec91013f4f3eeaf4c5214f6674074
dirudeen <dirudeen22@gmail.com>
2024-12-26 14:14:54 +0300

    Add click handler to send a post resquest to checkout endpoint to initiate the course purchase process

--- src/app/(course)/courses/[courseId]/chapters/[chapterId]/_components/courseEnrollButton.tsx
+++ src/app/(course)/courses/[courseId]/chapters/[chapterId]/_components/courseEnrollButton.tsx
@@ -1,20 +1,54 @@
-"use client"
+"use client";
 
+import { Spinner } from "@/components/Spinner";
 import { Button } from "@/components/ui/button";
 import { formatPrice } from "@/lib/format";
+import { Island_Moments } from "next/font/google";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 interface CourseEnrollBottonProps {
-    courseId: string;
-    price: number;
+  courseId: string;
+  price: number;
 }
 
-export default function CourseEnrollBotton({courseId, price}: CourseEnrollBottonProps) {
+export default function CourseEnrollBotton({
+  courseId,
+  price,
+}: CourseEnrollBottonProps) {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleClick = async () => {
+    setIsLoading(true);
+    try {
+      const res = await fetch(`/api/course/${courseId}/checkout`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (!res.ok) {
+        throw new Error("Something went wrong");
+      }
+      const { url } = (await res.json()) as { url: string };
+      window.location.assign(url);
+    } catch (error) {
+      if (error instanceof Error) {
+        toast.error(error.message);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <Button
-    size={"sm"}
-    className="w-full md:w-auto"
+      onClick={handleClick}
+      size={"sm"}
+      className="w-full md:w-auto"
+      disabled={isLoading}
     >
-        Enroll for {formatPrice(price)}
+      Enroll for {formatPrice(price)}
     </Button>
-  )
+  );
 }
